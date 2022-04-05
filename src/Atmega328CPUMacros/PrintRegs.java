2f46516c84d182931fe1e27178bedddf5559614a
Evin Jaff <evin@wustl.edu>
2022-04-04 19:23:29 -0500

    Base class added

---
+++ src/Atmega328CPUMacros/PrintRegs.java
@@ -0,0 +1,30 @@
+package Atmega328CPUMacros;
+
+import Interpreter.AbstractCPU;
+import Interpreter.AbstractInstruction;
+
+public class PrintRegs extends AbstractInstruction {
+
+	public PrintRegs() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setArgs(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public AbstractCPU run(AbstractCPU cpu, boolean debug) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
