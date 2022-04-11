4e268875bf4411be2a9395d180add7c44445bb95
udirose <udirose@wustl.edu>
2022-04-11 13:40:53 -0500

    more extensive ADD tests

--- src/Atmega328CPUTest/TestADD.java
+++ src/Atmega328CPUTest/TestADD.java
@@ -15,7 +15,6 @@ public class TestADD {
 
     @Test
     void testADD_Simple() {
-
         ASMFileReader AFR = new ASMFileReader("src/Atmega328CPUInstructionsTest/AssemblyFiles/ADDSimple.S");
         AFR.read();
         AbstractCPU ArduinoUno = new ATmega328PCPU();
@@ -29,6 +28,63 @@ public class TestADD {
         }
         int correctVal = (byte)ArduinoUno.getRegister("r28") + oldDest;
         assertEquals(ArduinoUno.getRegister("r29"), correctVal);
+    }
+
+    @Test
+    void testADD_AllRegs() {
+        ASMFileReader AFR = new ASMFileReader("src/Atmega328CPUInstructionsTest/AssemblyFiles/ADDFull.S");
+        AFR.read();
+        AbstractCPU ArduinoUno = new ATmega328PCPU();
+        System.out.println(AFR.getAllParsedLines());
+
+        //test add for regs 16-31
+        for(int i = 16; i < 31; i++){
+            String destReg = "r"+i;
+            int oldDest = (byte)ArduinoUno.getRegister(destReg);
+            String srcReg = "r"+(i+1);
+            try {
+                ArduinoUno.run(AFR.getAllParsedLines());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            int correctVal = (byte)ArduinoUno.getRegister(srcReg) + oldDest;
+            assertEquals(ArduinoUno.getRegister(destReg), correctVal);
+        }
+    }
+
+    @Test
+    void testADD_Self(){
+        ASMFileReader AFR = new ASMFileReader("src/Atmega328CPUInstructionsTest/AssemblyFiles/ADDSelf.S");
+        AFR.read();
+        AbstractCPU ArduinoUno = new ATmega328PCPU();
+        System.out.println(AFR.getAllParsedLines());
+
+        int oldDest = (byte)ArduinoUno.getRegister("r28");
+        try {
+            ArduinoUno.run(AFR.getAllParsedLines());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int correctVal = (byte)ArduinoUno.getRegister("r28") + oldDest;
+        assertEquals(ArduinoUno.getRegister("r28"), correctVal);
+    }
+
+    @Test
+    void testADD_Overflow() {
+        ASMFileReader AFR = new ASMFileReader("src/Atmega328CPUInstructionsTest/AssemblyFiles/ADDOverflow.S");
+        AFR.read();
+        AbstractCPU ArduinoUno = new ATmega328PCPU();
+        System.out.println(AFR.getAllParsedLines());
+
+        int oldDest = (byte)ArduinoUno.getRegister("r29");
+        try {
+            ArduinoUno.run(AFR.getAllParsedLines());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int correctVal = (byte)ArduinoUno.getRegister("r28") + oldDest;
+        assertEquals(ArduinoUno.getRegister("V"), 4);
     }
     
 }
