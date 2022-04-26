5af6955cc6a008d842446e77d8062bd9ae1ed2bb
udirose <udirose@wustl.edu>
2022-04-26 12:28:03 -0500

    completed addi functionality and unit tests pass

--- src/Atmega328CPUTest/TestADDI.java
+++ src/Atmega328CPUTest/TestADDI.java
@@ -53,21 +53,48 @@ public class TestADDI {
             fail("Exception Occured During Parsing");
         }
         AbstractCPU ArduinoUno = new ATmega328PCPU();
-
-        //test add for regs 16-31
-        for(int i = 16; i < 31; i++){
-            String destReg = "r"+i;
-            int oldDest = ArduinoUno.getRegister(destReg);
-            try {
-                ArduinoUno.loadProgram(AFR.getAllParsedLines());
-                ArduinoUno.run("main");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //checks if add value is correct
-            int correctVal = 4 + oldDest;
-            assertEquals(ArduinoUno.getRegister(destReg), correctVal);
+        int old16 = ArduinoUno.getRegister("r16");
+        int old17 = ArduinoUno.getRegister("r17");
+        int old18 = ArduinoUno.getRegister("r18");
+        int old19 = ArduinoUno.getRegister("r19");
+        int old20 = ArduinoUno.getRegister("r20");
+        int old21 = ArduinoUno.getRegister("r21");
+        int old22 = ArduinoUno.getRegister("r22");
+        int old23 = ArduinoUno.getRegister("r23");
+        int old24 = ArduinoUno.getRegister("r24");
+        int old25 = ArduinoUno.getRegister("r25");
+        int old26 = ArduinoUno.getRegister("r26");
+        int old27 = ArduinoUno.getRegister("r27");
+        int old28 = ArduinoUno.getRegister("r28");
+        int old29 = ArduinoUno.getRegister("r29");
+        int old30 = ArduinoUno.getRegister("r30");
+        int old31 = ArduinoUno.getRegister("r31");
+        
+        try {
+            ArduinoUno.loadProgram(AFR.getAllParsedLines());
+            ArduinoUno.run("main");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        
+        
+        assertEquals(ArduinoUno.getRegister("r16"), old16+4);
+        assertEquals(ArduinoUno.getRegister("r17"), old17+4);
+        assertEquals(ArduinoUno.getRegister("r18"), old18+4);
+        assertEquals(ArduinoUno.getRegister("r19"), old19+4);
+        assertEquals(ArduinoUno.getRegister("r20"), old20+4);
+        assertEquals(ArduinoUno.getRegister("r21"), old21+4);
+        assertEquals(ArduinoUno.getRegister("r22"), old22+4);
+        assertEquals(ArduinoUno.getRegister("r23"), old23+4);
+        assertEquals(ArduinoUno.getRegister("r24"), old24+4);
+        assertEquals(ArduinoUno.getRegister("r25"), old25+4);
+        assertEquals(ArduinoUno.getRegister("r26"), old26+4);
+        assertEquals(ArduinoUno.getRegister("r27"), old27+4);
+        assertEquals(ArduinoUno.getRegister("r28"), old28+4);
+        assertEquals(ArduinoUno.getRegister("r29"), old29+4);
+        assertEquals(ArduinoUno.getRegister("r30"), old30+4);
+        assertEquals(ArduinoUno.getRegister("r31"), old31+4);
+       
     }
 
     @Test
@@ -94,56 +121,6 @@ public class TestADDI {
         assertEquals(0x81, ArduinoUno.getRegister("r29"));
         assertEquals(ArduinoUno.getRegister("V"), 1);
     }
-
-//    @Test
-//    void testADDI_TwosComplementOverflow() {
-//        ASMFileReader AFR = new ASMFileReader("src/Atmega328CPUInstructionsTest/AssemblyFiles/ADDTwosComplementOverflow.S");
-//        try {
-//            AFR.read();
-//        } catch (FileNotFoundException | AssemblyParserException e1) {
-//            // TODO Auto-generated catch block
-//            e1.printStackTrace();
-//            fail("Exception Occured During Parsing");
-//        }
-//        AbstractCPU ArduinoUno = new ATmega328PCPU();
-//        ArduinoUno.enableDebug(true);
-//
-//        try {
-//            ArduinoUno.loadProgram(AFR.getAllParsedLines());
-//            ArduinoUno.run("main");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        //check register is over 127 and V is set to 1
-//        assertEquals(0x8E, ArduinoUno.getRegister("r29"));
-//        assertEquals(ArduinoUno.getRegister("V"), 1);
-//    }
-//
-//    @Test
-//    void testADDI_RealOverflow() {
-//        ASMFileReader AFR = new ASMFileReader("src/Atmega328CPUInstructionsTest/AssemblyFiles/ADDRealOverflow.S");
-//        try {
-//            AFR.read();
-//        } catch (FileNotFoundException | AssemblyParserException e1) {
-//            // TODO Auto-generated catch block
-//            e1.printStackTrace();
-//            fail("Exception Occured During Parsing");
-//        }
-//        AbstractCPU ArduinoUno = new ATmega328PCPU();
-//        ArduinoUno.enableDebug(true);
-//
-//        try {
-//            ArduinoUno.loadProgram(AFR.getAllParsedLines());
-//            ArduinoUno.run("main");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        //check register is over 127 and V is set to 1
-//        assertEquals(0x0E, ArduinoUno.getRegister("r29"));
-//
-//    }
 
 }
 
